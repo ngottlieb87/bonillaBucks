8d0b1e80bfbc9ed89558d43a38e425de21b1323c
ngottlieb87 <nogpdx@gmail.com>
2017-10-07 18:45:53 -0700

    passing validation spec for username

--- lib/Student.rb
+++ lib/Student.rb
@@ -2,4 +2,5 @@ class Student < ActiveRecord::Base
   has_many :accounts
   has_many :rewards, through: :accounts
   has_many :fines, through: :accounts
+  validates(:username, {:presence=> true, :length=>{:maximum=>30}})
 end
--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -3,4 +3,12 @@ require("spec_helper")
 describe(Student) do
   it{ should have_many(:fines) }
   it{ should have_many(:rewards) }
+
+  it ("validates username presence and length") do
+    test_user1 = Student.new({:username => ""})
+    test_user2 = Student.new({:username => "heylookatthatpigitssuperbiganduglyandsmellslikeshitandstuffohmygodtheyrecominginhelp"})
+    expect(test_user1.save).to eq(false)
+    expect(test_user2.save).to eq(false)
+  end
+
 end
